d82f361b25428835d83be69772d53fa0cf09fe99
t-wen <30753500+t-wen@users.noreply.github.com>
2018-08-01 17:23:35 +0800

    update

--- index.js
+++ index.js
@@ -50,6 +50,60 @@ router.get('/signin',async(ctx) => {
 	connection.end();
 });
 
+router.get('/signup',async (ctx) => {
+	
+	var mysql = require('mysql');
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '661475chen',
+		database: 'my_db'
+	});
+
+	connection.connect(function (err) {
+		if (err) {
+			console.error('error connecting: ' + err.stack);
+			return;
+		}
+
+		console.log('connected as id ' + connection.threadId);
+	});
+
+	var response = {
+		"name": ctx.query.name,
+		"password": ctx.query.password,
+		"firstname": ctx.query.firstname,
+		"lastname": ctx.query.lastname,
+		"birthday": ctx.query.birthday,
+		"sex": ctx.query.sex,
+		"age": ctx.query.age,
+		"wechart": ctx.query.wechart,
+		"qq": ctx.query.qq,
+		"email": ctx.query.email,
+		"country": ctx.query.country,
+		"address": ctx.query.address,
+		"phone": ctx.query.phone,
+		"websize": ctx.query.websize,
+		"github": ctx.query.github,
+		"bio": ctx.query.bio
+	};
+
+	var addSql = 'INSERT INTO ArthurSlogAccount(AccountName, Password, Firstname, Lastname, Birthday, Sex, Age, Wechart, Qq, Email, Country, Address, Phone, Websize, Github, Bio) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+	var addSqlParams = [response.name, response.password, response.firstname, response.lastname, response.birthday, response.sex, response.age, response.wechart, response.qq, response.email, response.country, response.address, response.phone, response.websize, response.github, response.bio];
+
+	ctx.body = await new Promise((resolve, reject) => {
+		connection.query(addSql,addSqlParams,function (err, result) {
+			if(err){
+				reject(err);
+				console.log('[INSERT ERROR] - ',err.message);
+				return;
+			}
+			resolve('Signup Successful!');
+		});
+	});
+	connection.end();
+});
+
 app.use(router.routes());
 
 app.listen(3000);
