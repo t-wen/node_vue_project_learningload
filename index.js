6275718f1b8ce2c13b8c7e16f5cf86249abd3bee
t-wen <30753500+t-wen@users.noreply.github.com>
2018-08-03 18:26:09 +0800

    update

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router');
 const router = new Router();
+
+// $ GET /package.json
 app.use(serve('.'));
 
-router.get('/signin',async(ctx) => {
+//登陆模块 signin()
+router.get('/signin', async (ctx) => {
+
 	var mysql = require('mysql');
 	var connection = mysql.createConnection({
 		host: 'localhost',
@@ -14,6 +18,7 @@ router.get('/signin',async(ctx) => {
 		database: 'my_db'
 	});
 
+
 	connection.connect(function (err) {
 		if (err) {
 			console.error('error connecting: ' + err.stack);
@@ -21,37 +26,40 @@ router.get('/signin',async(ctx) => {
 		}
 		console.log('connected as id ' + connection.threadId);
 	});
+
 
 	var response = {
 		"name": ctx.query.name,
 		"password": ctx.query.password
 	};
 
-	var addSql = 'SELECT * FROM Account WHERE AccountName=?';
+	var addSql = 'SELECT * FROM ArthurSlogAccount WHERE AccountName=?';
 	var addSqlParams = [response.name];
-	
+
 	ctx.body = await new Promise((resolve, reject) => {
+
 		connection.query(addSql, addSqlParams, function (err, result) {
 			if (err) {
 				reject(err);
 				console.log('[SELECT ERROR] - ', err.message);
 				return;
 			}
-			if(result[0].Password == response.password) {
+			if (result[0].Password == response.password) {
 				resolve(result[0]);
-				console.log('Welcome~ SignIn Successful ^_^' + '\\' + 'Level: ' + result[0].Level + ' Houses: ' + result[0].Houses);
+				console.log('Welcome~ SingIn Successul ^_^' + '\\' + 'Level: ' + result[0].Level + ' Houses: ' + result[0].Houses);
 			}
 			if (result[0].Password != response.password) {
-				reject('SignIn Fault ^_^!');
-				console.log('SignIn Fault ^_^!');
+				reject('SingIn Fault ^_^!');
+				console.log('SingIn Fault ^_^!');
 			}
 		});
 	});
 	connection.end();
 });
 
-router.get('/signup',async (ctx) => {
-	
+//注册模块 signup() 
+router.get('/signup', async (ctx) => {
+
 	var mysql = require('mysql');
 	var connection = mysql.createConnection({
 		host: 'localhost',
@@ -60,14 +68,15 @@ router.get('/signup',async (ctx) => {
 		database: 'my_db'
 	});
 
+
 	connection.connect(function (err) {
 		if (err) {
 			console.error('error connecting: ' + err.stack);
 			return;
 		}
-
 		console.log('connected as id ' + connection.threadId);
 	});
+
 
 	var response = {
 		"name": ctx.query.name,
@@ -80,7 +89,7 @@ router.get('/signup',async (ctx) => {
 		"wechart": ctx.query.wechart,
 		"qq": ctx.query.qq,
 		"email": ctx.query.email,
-		"country": ctx.query.country,
+		"contury": ctx.query.contury,
 		"address": ctx.query.address,
 		"phone": ctx.query.phone,
 		"websize": ctx.query.websize,
@@ -88,20 +97,21 @@ router.get('/signup',async (ctx) => {
 		"bio": ctx.query.bio
 	};
 
-	var addSql = 'INSERT INTO ArthurSlogAccount(AccountName, Password, Firstname, Lastname, Birthday, Sex, Age, Wechart, Qq, Email, Country, Address, Phone, Websize, Github, Bio) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-	var addSqlParams = [response.name, response.password, response.firstname, response.lastname, response.birthday, response.sex, response.age, response.wechart, response.qq, response.email, response.country, response.address, response.phone, response.websize, response.github, response.bio];
+	var addSql = 'INSERT INTO ArthurSlogAccount(AccountName, Password, Firstname, Lastname, Birthday, Sex, Age, Wechart, Qq, Email, Contury, Address, Phone, Websize, Github, Bio) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+	var addSqlParams = [response.name, response.password, response.firstname, response.lastname, response.birthday, response.sex, response.age, response.wechart, response.qq, response.email, response.contury, response.address, response.phone, response.websize, response.github, response.bio];
+
+
 
 	ctx.body = await new Promise((resolve, reject) => {
 
-        connection.query(addSql,addSqlParams,function (err, result) {
-            if(err){
-                reject(err);
-                console.log('[INSERT ERROR] - ',err.message);
-                return;
-            }
-            resolve('Singup Successful!');
-        });
-
+		connection.query(addSql, addSqlParams, function (err, result) {
+			if (err) {
+				reject(err);
+				console.log('[INSERT ERROR] - ', err.message);
+				return;
+			}
+			resolve('Singup Successful!');
+		});
 	});
 	connection.end();
 });
@@ -109,4 +119,5 @@ router.get('/signup',async (ctx) => {
 app.use(router.routes());
 
 app.listen(3000);
+
 console.log('listening on port 3000');
